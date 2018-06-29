2af609a61610ea061d05ca74716f782ec67c0d29
Étienne Noss <etienne.noss@wifirst.fr>
2018-06-29 15:38:55 +0200

    Add variable substitution support in commands, start refactoring

--- gitlab-webhook-receiver.py
+++ gitlab-webhook-receiver.py
@@ -4,10 +4,14 @@
 """ Gitlab Webhook Receiver """
 # Based on: https://github.com/schickling/docker-hook
 
+import sys
+import logging
 import json
-import yaml
 import subprocess
+
+from string import Template
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
 try:
     # For Python 3.0 and later
     from http.server import HTTPServer
@@ -16,8 +20,10 @@ except ImportError:
     # Fall back to Python 2
     from BaseHTTPServer import BaseHTTPRequestHandler
     from BaseHTTPServer import HTTPServer as HTTPServer
-import sys
-import logging
+
+import yaml
+
+CONFIG = None
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.DEBUG,
@@ -34,41 +40,67 @@ class RequestHandler(BaseHTTPRequestHandler):
         header_length = int(self.headers.getheader('content-length', "0"))
         json_payload = self.rfile.read(header_length)
         json_params = {}
-        if len(json_payload) > 0:
+        if json_payload:
             json_params = json.loads(json_payload)
 
-        # get gitlab secret token
-        gitlab_token_header = self.headers.getheader('X-Gitlab-Token')
-
-        # get project homepage
+        # get project configuration
         project = json_params['project']['homepage']
-
         try:
-            # get command and token from config file
-            command = config[project]['command']
-            gitlab_token = config[project]['gitlab_token']
-
-            logging.info("Load project '%s' and run command '%s'", project, command)
-        except KeyError as err:
-            self.send_response(500, "KeyError")
-            logging.error("Project '%s' not found in %s", project, args.cfg)
+            project_config = CONFIG[project]
+        except KeyError as ker:
+            self.send_response(400, "Unknown project %r" % ker)
             self.end_headers()
             return
 
-        # Check if the gitlab token is valid
-        if gitlab_token_header == gitlab_token:
-            logging.info("Start executing '%s'" % command)
-            try:
-                # run command in background
-                subprocess.Popen(command)
-                self.send_response(200, "OK")
-            except OSError as err:
-                self.send_response(500, "OSError")
-                logging.error("Command could not run successfully.")
-                logging.error(err)
-        else:
-            logging.error("Not authorized, Gitlab_Token not authorized")
-            self.send_response(401, "Gitlab Token not authorized")
+        # Fetch the project's gitlab token in configuration
+        try:
+            gitlab_token = project_config['gitlab_token']
+        except KeyError as ker:
+            self.send_response(500, "No gitlab token in project configuration")
+            self.end_headers()
+            return
+
+        # Check it
+        if gitlab_token != self.headers.getheader('X-Gitlab-Token'):
+            self.send_response(401, "Invalid token")
+            self.end_headers()
+            return
+
+        # A dict of values that can be substitued on the command line
+        available_params = {
+            "event": json_params.get("object_kind"),
+            "sha": json_params.get("checkout_sha"),
+            "user": json_params.get("user_username"),
+            "ref": json_params.get("ref"),
+            "project_name": json_params["project"]["name"],
+            "project_owner": json_params["project"]["namespace"],
+        }
+        logging.debug("Available variable substitions: %s", available_params)
+
+        # Get the command to call
+        try:
+            # Substitute $variable => available_params["variable"]
+            command = [Template(i).substitute(**available_params)
+                       for i in project_config.get("command", ())]
+        except KeyError as err:
+            self.send_response(500, "Invalid substitution %s in command" % err)
+            self.end_headers()
+            self.wfile.write("Available substitutions: %s\n"
+                             % ", ".join(available_params))
+            return
+        if not command:
+            self.send_response(500, "No command defined for project")
+            self.end_headers()
+            return
+
+        logging.info("Calling %s", " ".join(command))
+        try:
+            subprocess.Popen(command)
+        except OSError as ose:
+            self.send_response(500, "Could not call command: %s" % ose)
+            self.end_headers()
+            return
+        self.send_response(200, "Hook command called")
         self.end_headers()
 
 
@@ -85,7 +117,6 @@ def get_parser():
                         dest="port",
                         type=int,
                         default=8666,
-                        metavar="PORT",
                         help="port where it listens")
     parser.add_argument("--cfg",
                         dest="cfg",
@@ -94,26 +125,22 @@ def get_parser():
     return parser
 
 
-def main(addr, port):
-    """Start a HTTPServer which waits for requests."""
-    httpd = HTTPServer((addr, port), RequestHandler)
-    httpd.serve_forever()
-
-
-if __name__ == '__main__':
+def main():
+    global CONFIG  # FIXME XXX TODO YUCK
     parser = get_parser()
 
-    if len(sys.argv) == 0:
-        parser.print_help()
-        sys.exit(1)
     args = parser.parse_args()
 
     # load config file
     try:
         with open(args.cfg, 'r') as stream:
-            config = yaml.load(stream)
+            CONFIG = yaml.load(stream)
     except IOError as err:
-        logging.error("Config file %s could not be loaded", args.cfg)
+        logging.error("Config file %s could not be loaded: %s", args.cfg, err)
         sys.exit(1)
+    httpd = HTTPServer((args.addr, args.port), RequestHandler)
+    httpd.serve_forever()
 
-    main(args.addr, args.port)
+
+if __name__ == '__main__':
+    main()
